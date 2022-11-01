7f9d1cb0a6bc9df9f855862cc18cad148f143a25
SergBobyr <serg_bobyr@ukr.net>
2022-11-01 19:26:50 +0200

    one-hw-js

---
+++ js/lesson1/main.js
@@ -0,0 +1,41 @@
+
+let a ='hello';
+console.log(a);
+let b= 'owu';
+console.log(b);
+let c ='com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+let i = 3.14;
+console.log(i);
+let j = 2.7;
+console.log(j);
+let k = 16;
+console.log(k);
+let l = true;
+console.log(l);
+let m = false;
+console.log(m);
+let firstName = 'Sergej';
+let middleName = 'Aleksandrovich';
+let lastName = 'Bobyr';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+let o = 100;
+console.log(typeof o);
+let p = '100';
+console.log(typeof p);
+let q = true;
+console.log(typeof q);
+let firstNamePrompt = prompt('your first name')
+let middleNamePrompt = prompt('your middle name')
+let lastNamePrompt = prompt('your last name')
